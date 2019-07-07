0165db3bb5f36ac8bd0ef24dc4f7eda033da3a5d
zzw <1262534317@qq.com>
2019-07-07 13:34:35 +0800

    update

--- hs/src/views/Index.vue
+++ hs/src/views/Index.vue
@@ -1,26 +1,24 @@
 <template>
     <div>
         index
-        <mt-tab-container v-model="selected">
-            <mt-tab-container-item id="1">
+        <mt-tab-container v-model="value">
+            <mt-tab-container-item id="tab1">
                 内容1 <i class="iconfont icon-shouye"></i>
             </mt-tab-container-item>
-            <mt-tab-container-item id="2">
+            <mt-tab-container-item id="tab2">
                 内容二 <i class="iconfont icon-shoujihao"></i>
             </mt-tab-container-item>
-            <mt-tab-container-item id="3">
+            <mt-tab-container-item id="tab3">
                 内容3   <i class="iconfont icon-user-circle-o"></i>
             </mt-tab-container-item>
-            <mt-tab-container-item id="4">
+            <mt-tab-container-item id="tab4">
                 内容4   <i class="iconfont icon-wode"></i>
             </mt-tab-container-item>
         </mt-tab-container>
         <mt-tabbar v-model="selected" fixed>
-            <mt-tab-item v-for="(el,i) in nav" :key="i" :id='el.id' :data-index="el.id">
-                <div @click="reto()" :data-index="el.id">
+            <mt-tab-item v-for="(el,i) in nav" :key="i" :id="el.id" :data-index="el.id">       
                     {{el.title}}{{el.id}}
-                </div>
-             </mt-tab-item>
+            </mt-tab-item>
         </mt-tabbar>
     </div>
 </template>
@@ -29,8 +27,14 @@
 export default {
     data(){
         return  {
-            selected:"1",
-            nav:[]
+            selected:"",
+            value:"tab1",
+            nav:[],
+        }
+    },
+    watch:{
+        selected(){
+           this.value=`tab${this.selected}`
         }
     },
     methods:{
@@ -43,11 +47,7 @@ export default {
                 this.nav=res.data.nav
                 console.log(this.nav)
             })
-        },
-         reto(e){
-        console.log(e.target)
         }
-
     },
     created(){
        this.loadMore()
